ec31739730e9a5cd6ef2eba04a630550108bbfb7
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2020-03-26 22:47:28 +0100

    Initial project structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Let the file sound</h1>
+    </header>
+    <main>
+      <p>Hello World</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
