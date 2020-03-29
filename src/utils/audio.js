491be194b31cd3c2dc0472a6a300a824f7ad098c
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2020-03-29 17:52:54 +0200

    Use webkitAudioContext and decodeAudioData without promises

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,10 +1,16 @@
 export function decodeAudioFile(audioFileBuffer) {
-  const context = new window.AudioContext();
-  return context.decodeAudioData(audioFileBuffer);
+  return new Promise((resolve, reject) => {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const context = new AudioContext();
+    // Use callbacks instead of Promise syntax because it is not supported in Safari
+    // https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/decodeAudioData
+    return context.decodeAudioData(audioFileBuffer, resolve, reject);
+  });
 }
 
 export function playAudioFile(audioBuffer) {
-  const context = new window.AudioContext();
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const context = new AudioContext();
   const source = context.createBufferSource();
   source.buffer = audioBuffer;
   source.loop = false;
--- src/utils/files.js
+++ src/utils/files.js
@@ -1,3 +1,5 @@
+// Use this function because file.arrayBuffer() is not supported in all platforms (Safar, Firefox for Android...)
+// https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
 export function readFileAsArrayBuffer(file) {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
